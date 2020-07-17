61132c527b8eef329be21c7c8bfe3c98bf404f61
Rohit Saini <sketchlearn7@gmail.com>
2020-07-17 15:33:47 +0530

    Code Updated

--- SwiftUI11/ContentView.swift
+++ SwiftUI11/ContentView.swift
@@ -8,9 +8,28 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State var currentPage = 0
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Picker(selection: $currentPage, label: Text("")){
+                Image(systemName: "house.fill").tag(0)
+                Image(systemName: "flame.fill").tag(1)
+                Image(systemName: "paperplane.fill").tag(2)
+            }.pickerStyle(SegmentedPickerStyle())
+            Spacer()
+            if currentPage == 0{
+                page1()
+            }
+            if currentPage == 1{
+                page2()
+            }
+            if currentPage == 2{
+                page3()
+            }
+        }
+        
     }
 }
 
@@ -19,3 +38,23 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct page1:View{
+    var body: some View{
+        Text("Page1")
+    }
+}
+
+struct page2:View{
+    var body: some View{
+        Text("Page2")
+    }
+}
+
+struct page3:View{
+    var body: some View{
+        Text("Page3")
+    }
+}
+
+
